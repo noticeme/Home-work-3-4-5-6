321a86fe60ea57895b75c3f26913307a82d1d765
noticeme <artyrizcherei@gmail.com>
2020-12-23 18:36:26 +0300

    ready

---
+++ main/main.js
@@ -0,0 +1,35 @@
+// ЗАДАНИЕ 3
+
+// function checked(){
+//     str.startsWith(searchString[, position])
+// }
+// checked()
+
+
+
+
+
+
+
+// ЗАДАНИЕ 4-5
+
+var string = 'aajlkjjskdjfAlsj;gkejflksajfjskdaaaaAAA';
+
+document.write(string.match(/a/gi).length);
+
+// RegEx*
+
+// coderoad.ru/2903542/JavaScript-сколько-раз-символ-встречается-в-строке
+
+
+
+
+
+
+// ЗАДАНИЕ 6
+
+function name(){
+    let now = Date();
+console.log( now );
+}
+name()
